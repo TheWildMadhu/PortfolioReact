a08213730ee2a1b3ff6d19d61f083a11497ce8f7
Madhu Sen <114171865+TheWildMadhu@users.noreply.github.com>
2023-10-18 08:13:31 +0530

    Update Landing.js

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -9,8 +9,8 @@ import { Link } from "react-router-dom"
         <img className="background-img" src="/img/tokenstreet-vbPk2tB2HrU-unsplash.jpg" alt="img"/>
         </div>
         <div className="content">
-            <p>Hi,I'm Aman krishan.</p>
-            <h1> React developer. </h1>
+            <p>Hi,I'm Madhu Sen.</p>
+            <h1> Frontend developer. </h1>
             <div>
                 <Link to="/project" className="btnn">Project</Link>
             </div>
