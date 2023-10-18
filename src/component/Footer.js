01e9a48ec5d7defe2d324ac2997bac3627191577
Madhu Sen <114171865+TheWildMadhu@users.noreply.github.com>
2023-10-18 08:21:14 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -11,37 +11,15 @@ const Footer = () => {
     <div className="footer-content">
       <h3>welcome To my portfolio</h3>
       <p>
-        Hello, My name is Aman krishan.
-        you have come this far it means you like my website.
-        thank you
+        Hello, I'm Madhu Sen.
+        You have come this far, it means you like my website.
+        Thank you
       </p>
-      <ul className="socials">
-        <li>
-          <a href="https://m.facebook.com/100007803135881/">
-            <AiFillFacebook/>
-          </a>
-        </li>
-        <li>
-      
-          <a href="https://twitter.com/krishan_aman14">
-            <AiFillTwitterSquare/>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Amankrishan14">
-          <AiFillGithub/>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/aman-krishan-983980230/">
-            <AiFillLinkedin/>
-          </a>
-        </li>
-      </ul>
+ 
     </div>
     <div className="footer-bottom">
       <p>
-        copyright &copy;2021 <a href="#">Ak</a>{" "}
+        copyright &copy;2021 <a href="#">Madhu Sen</a>{" "}
       </p>
     </div>
     <div className="footer-menu">
